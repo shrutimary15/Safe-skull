1a43a163c6e1c8213e33a89132b0e2584c4e6a29
Shanu Mathew <shanutmathew1703@gmail.com>
2023-05-12 17:46:09 +0530

    Adding Frontend

---
+++ static/script.js
@@ -0,0 +1,7 @@
+const selectImage = document.querySelector('.select-image');
+const inputFile = document.querySelector('#file');
+
+selectImage.addEventListener('click',function () {
+    inputFile.click();
+})
+
