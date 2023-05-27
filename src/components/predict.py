fd90e261bb750dbbfe75fef7db3966ec0399e3dd
shrutimary15 <shanshru15@gmail.com>
2023-05-27 19:10:38 +0530

    Changes to save predictions

--- src/components/predict.py
+++ src/components/predict.py
@@ -1,8 +1,6 @@
 from ultralytics import YOLO
-import glob
 import os
 import shutil
-import subprocess
 
 
 directory_path = "runs/detect/predict"
@@ -10,14 +8,17 @@ if os.path.exists(directory_path):
     shutil.rmtree(directory_path)
 
 
-img_path = 'Bike-Helmet-Detection-2/test/images'
-image_path='D:\Documents\Projects\AI-Camera-Implementation\Testdata\Helmet.jpg'
-command='yolo task=detect mode=predict model=runs/detect/train/weights/best.pt conf=0.5 source={}'.format(image_path)
-output = subprocess.check_output(command.split())
-print('Needed Result: ',output)
-num_objects = output.decode('utf-8').count('Helmet')
-
-print('Number of objects detected: {}'.format(num_objects))
+img_path = 'Bike-Helmet-Detection-2/test/images/BikesHelmets739_png_jpg.rf.63df6a974579d37a25c27ccff6900fb5.jpg'
+os.system('yolo task=detect mode=predict model=runs/detect/train/weights/best.pt conf=0.5 source={}'.format(img_path))
+output_file = 'results.txt'
+model = YOLO('runs/detect/train/weights/best.pt')
+predictions = model(img_path, save_txt=None)
+#{1 : without helmet, 0: with helmet}
+with open("runs/detect/predict/predicted_labels.txt", '+w') as file:
+      for idx, prediction in enumerate(predictions[0].boxes.xywhn): # change final attribute to desired box format
+          cls = int(predictions[0].boxes.cls[idx].item())
+          # Write line to file in YOLO label format : cls x y w h
+          file.write(f"{cls}, {prediction[0].item()}, {prediction[1].item()}, {prediction[2].item()}, {prediction[3].item()}\n")
 
 
 
